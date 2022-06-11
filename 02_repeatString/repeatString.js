e6e0e933319ed1a3a42c7b506bfcaeff68641109
Declan <declan.watt@outlook.com>
2022-06-11 17:24:37 +1200

    Adds logic to fix return value when Numrepeats = 0

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,14 +2,18 @@ const repeatString = (str, numRepeats) => {
  
 	let finalString = '';
 
-	if(numRepeats < 1){
+	if(numRepeats < 1 && numRepeats != 0){
   	return "ERROR";
     }
-		for(let i = 0; i < numRepeats; i ++){
+
+	for(let i = 0; i < numRepeats; i ++){
     	finalString += str;
     } 
+
 	return finalString;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
