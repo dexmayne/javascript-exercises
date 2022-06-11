904e227b7749bc7611ec9aca65d534520a81f43f
Declan <declan.watt@outlook.com>
2022-06-11 17:44:57 +1200

    Add solution to test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(arr) {
+   
+    let x = arr.split("");
+    let newString = x.reverse().join('');
+    
+    return newString;
 };
 
 // Do not edit below this line
