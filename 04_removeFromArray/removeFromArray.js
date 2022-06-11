f64cec7899b92bab07e8761afdb3a3058368c328
Declan <declan.watt@outlook.com>
2022-06-12 11:56:03 +1200

    add solution using Filter function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,5 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = (userArray, ...toRemove) => 
+	userArray.filter(e => !toRemove.includes(e));
 
 // Do not edit below this line
 module.exports = removeFromArray;
