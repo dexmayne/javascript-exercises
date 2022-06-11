65fe9ec27a21e72a67ab9205f8919b1eae3ac8fd
Declan <declan.watt@outlook.com>
2022-06-11 17:06:12 +1200

    Add solution to repeatString functio

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = (str, numRepeats) => {
+ 
+	let finalString = '';
 
+	if(numRepeats < 1){
+  	return "ERROR";
+    }
+		for(let i = 0; i < numRepeats; i ++){
+    	finalString += str;
+    } 
+	return finalString;
 };
 
 // Do not edit below this line
