d08ddc707fe9a0a31fd8c3a1758219927cd3f8b2
Declan <declan.watt@outlook.com>
2022-06-12 12:00:21 +1200

    Add solution using for loop and if conditionals

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
-};
-
+    if(isNaN(num1) || isNaN(num2) || num1 < 0 || num2 < 0 || typeof num1 === 'string' || typeof num2 === 'string'){
+        return "ERROR";
+    }
+    
+     let min = Math.min(num1, num2);
+     let max = Math.max(num1, num2);
+     let finalSum = 0;
+      
+      for(let i = min; i <= max; i++){
+          finalSum += i;
+      } 
+      return finalSum;
+    }
+    
 // Do not edit below this line
 module.exports = sumAll;
